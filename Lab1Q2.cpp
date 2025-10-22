76ad6fae3d20f0d4f907bf4077a549759547640f
Rezwan Ahmed Prottoy <rezwan@Rezwans-MacBook-Pro.local>
2025-10-22 20:20:53 +0600

    test

--- Lab1Q2.cpp
+++ Lab1Q2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Hello
+//Hello//tes
 bool isOperator(char ch) {
     return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '=');
 }
